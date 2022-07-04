ee747e84db4166e58be39114f9a2cce48568aa6d
RodrigoFNascimento <rodrigofn0@hotmail.com>
2022-07-03 21:25:22 -0300

    Add GetValueOrDefault

--- AppSetttingsReader/AppSettings.cs
+++ AppSetttingsReader/AppSettings.cs
@@ -17,7 +17,7 @@ namespace AppSetttingsReader
         /// <exception cref="ConfigurationErrorsException">
         /// Lançada quando a configuração está em formato inválido ou não existe.
         /// </exception>
-        public static string Get(string key)
+        public static string GetValue(string key)
         {
             string value = ConfigurationManager.AppSettings[key];
 
@@ -36,11 +36,30 @@ namespace AppSetttingsReader
         /// <exception cref="ConfigurationErrorsException">
         /// Lançada quando a configuração está em formato inválido ou não existe.
         /// </exception>
-        public static TValue Get<TValue>(string key)
+        public static TValue GetValue<TValue>(string key)
         {
-            var value = Get(key);
+            var value = GetValue(key);
 
             return (TValue)TypeDescriptor.GetConverter(typeof(TValue)).ConvertFromInvariantString(value);
+        }
+
+        /// <summary>
+        /// Obtém o valor de uma configuração do AppSettings ou valor default.
+        /// </summary>
+        /// <typeparam name="TValue">Tipo para o qual o valor deve ser convertido.</typeparam>
+        /// <param name="key">Identificador da configuração.</param>
+        /// <param name="defaultValue">Valor retornado caso não seja possível obter valor do AppSettings.</param>
+        /// <returns>Valor da configuração convertido.</returns>
+        public static TValue GetValueOrDefault<TValue>(string key, TValue defaultValue)
+        {
+            try
+            {
+                return GetValue<TValue>(key);
+            }
+            catch (Exception)
+            {
+                return defaultValue;
+            }
         }
 
         /// <summary>
@@ -55,11 +74,11 @@ namespace AppSetttingsReader
         /// <param name="key">Identificador da configuração.</param>
         /// <param name="value">Valor convertido da configuração.</param>
         /// <returns>true se <paramref name="key"></paramref> for convertido com sucesso; do contrário, false.</returns>
-        public static bool TryGet<TValue>(string key, out TValue value)
+        public static bool TryGetValue<TValue>(string key, out TValue value)
         {
             try
             {
-                value = Get<TValue>(key);
+                value = GetValue<TValue>(key);
                 return true;
             }
             catch (Exception)
